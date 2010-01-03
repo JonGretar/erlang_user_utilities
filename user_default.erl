9d6744354f64ec8fe00e722a87d2e02db696acc3
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2010-01-03 14:41:42 +0000

    Inital Import

---
+++ user_default.erl
@@ -0,0 +1,22 @@
+-module (user_default).
+-export ([sync/0,make/0,git/1]).
+
+%% Compiles all files in Emakefile and load into current shell.
+sync() ->
+	make:all([load]).
+
+% Run the make command in shell.
+make() ->
+	run_command(["make", "all"]).
+
+%% Run git command in shell.
+git(Command) ->
+	CommandList = ["git", Command],
+	run_command(CommandList).
+
+
+%%%% Private Functions
+
+run_command(CommandList) ->
+	Result = os:cmd(string:join(CommandList, " ")),
+	io:format("~s~n", [Result]).
